f19af704d72e7fc5cdc8c61efa68d43476073442
Alex Burnham <arburnha@colossus.it.mtu.edu>
2024-09-08 14:55:49 -0400

    made it work finally

---
+++ Hello.c
@@ -0,0 +1,8 @@
+//Hello world
+#include <stdio.h>
+int main(void) {
+printf("Hello, Alex Burnham! \n");
+	//prints message
+	    return 0;
+}
+   	
---
+++ hello.c
@@ -0,0 +1,8 @@
+//ahello.c program file
+#include <stdio.h>
+
+int main() {
+	printf("Hello world! this took a while didnt it\n");
+		return 0;
+}
+
